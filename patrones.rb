71ecd77e1d97bc118b8c8418f0ba80e36b2150a1
Constanza Mardones <coty.tiff@gmail.com>
2021-01-10 18:58:38 -0300

    letra x terminada

--- patrones.rb
+++ patrones.rb
@@ -28,14 +28,13 @@ end
 
 
 def letra_z(size)
-    
-    z = "*" * size + "\n"
     cont = 0
     sizez = size -2
-    until (sizez== cont) do
-        space = "\s"
+    
+    z = "*" * size + "\n"
+    until (sizez == cont) do
         
-            z += "#{space * sizez}*\n"
+            z += "#{"\s" * sizez}*\n"
             sizez -= 1
 
     end
@@ -44,4 +43,51 @@ def letra_z(size)
     return z
 end
 
-puts letra_z(n)
+def letra_x(size)
+    spa = "\s"
+    x = ""
+    y = size #contador
+    exspa = 0 #contador espacio
+
+    size.times do |i|
+        if i < (size / 2) 
+            x += "#{spa * exspa}*#{spa * (y - 2)}*\n"
+            y -= 2
+            exspa += 1
+
+        elsif i > (size / 2)
+            x += "#{spa * exspa}*#{spa * (y - 2)}*\n"
+            y += 2
+            exspa -= 1
+
+        else 
+            x += ("#{spa * (size / 2)}" + "*\n")
+            y = size / 2 + 1
+            exspa = 1
+
+        end
+    end
+    return x
+end
+
+def numero_cero(size)
+    o = '*' * size + "\n"
+
+    (size-2).times do
+        o += '*'
+        o += ("\s" * (size-2)) + "*\n"
+    end
+    o += '*' * size
+    
+    return o
+end
+
+# z += "#{"\s" * sizez}*\n"
+# sizez -= 1
+
+# puts letra_o(n)
+# puts letra_i(n)
+# puts letra_z(n)
+# puts letra_x(n)
+puts numero_cero(n)
+
