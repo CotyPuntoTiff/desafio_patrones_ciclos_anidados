5d611b3f98960f525f281c462e7f891e8b9e41c7
Constanza Mardones <coty.tiff@gmail.com>
2021-01-10 12:43:35 -0300

    metodo letra i

--- patrones.rb
+++ patrones.rb
@@ -1,1 +1,30 @@
-num = ARGV[0]
+n = ARGV[0].to_i
+
+def letra_o(size)
+
+    o = '*' * size + "\n"
+
+    (size-2).times do
+        o += '*'
+        o += ("\s" * (size-2)) + "*\n"
+    end
+    o += '*' * size
+    
+    return o
+end
+
+
+def letra_i(size)
+    
+    i = "*" * size + "\n"
+    
+    (size - 2).times do
+        i += ("\s" * (size / 2) + "*\n")
+    end
+    i += '*' * size
+    
+    return i
+end
+
+
+puts letra_i(n)
